b6746fc0f9ed13a0cc24ab489744c6c13dead76b
bitcoder <me@iamir.net>
2021-03-09 23:28:57 +0330

    Update iResponse.php

--- src/iapp/Http/Middleware/iResponse.php
+++ src/iapp/Http/Middleware/iResponse.php
@@ -29,7 +29,7 @@ class iResponse
         $log->model = isset($request->route()->controller) ? class_name($request->route()->getController()->model) : null;
         $log->action = isset($request->route()->action['as']) ? $request->route()->action['as'] : null;
         $log->endpoint = $request->url();
-        $log->_ip = $request->ip();
+        $log->_ip = $this->getUserIpAddr();
         $log->method = $request->method();
         $responses = [];
         $responses['request'] = count($request->toArray()) ? $request->toArray() : [];
@@ -64,5 +64,23 @@ class iResponse
                     ]);
             }
         }
+    }
+    public function getUserIpAddr(){
+        $ipaddress = '';
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = 'UNKNOWN';
+        return $ipaddress;
     }
 }
