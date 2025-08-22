49ac27f6af3fa188058568363d076eab932b4376
Amirhossein Jahani <me@iamir.net>
2025-08-22 23:49:24 +0330

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -7,6 +7,6 @@
  * Copyright (c) 2021. Powered by iamir.net
  */
 
-Route::namespace('v1')->prefix('v1')->middleware('auth:api')->group(function () {
+/*Route::namespace('v1')->prefix('v1')->middleware('auth:api')->group(function () {
     Route::apiResource('logs', 'ILogController', ['as' => 'api','except' => ['store','update','destroy']]);
-});
+});*/
